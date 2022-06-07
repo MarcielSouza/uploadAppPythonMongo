5e599c31875ddfec0c44bebfee679b3b8376bccb
Marciel <marciel.hsouza@hotmail.com>
2022-06-07 20:38:31 -0300

    Commit 2

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from .commands.userCommands import userComands
 
 def create_app():
     app = Flask(__name__)    
-    app.config["MONGO_URI"] = "mongodb+srv://marque:<password>@hands-on.fnmzd.mongodb.net/handsOn?retryWrites=true&w=majority"
+    app.config["MONGO_URI"] = "mongodb+srv://marque:1q2w3e4r@hands-on.fnmzd.mongodb.net/handsOn?retryWrites=true&w=majority"
     app.register_blueprint(usuario)
     app.register_blueprint(produto)
     app.register_blueprint(userComands)
