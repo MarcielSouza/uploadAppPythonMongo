b2ddb8882613b3fee97adec53ef04fc4339d524e
Marciel <marciel.hsouza@hotmail.com>
2022-06-07 18:43:25 -0300

    Commit - AppMongoPython

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
 from flask import Flask    #chama a biblioteca
+from .extensions import database
 from .routes.usuario import usuario
 from .routes.produto import produto
-from .extensions import database
-from .commands.userCommands import userCommands
+from .commands.userCommands import userComands
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__)    
     app.config["MONGO_URI"] = "mongodb+srv://marque:1q2w3e4r@hands-on.fnmzd.mongodb.net/handsOn?retryWrites=true&w=majority"
     app.register_blueprint(usuario)
     app.register_blueprint(produto)
